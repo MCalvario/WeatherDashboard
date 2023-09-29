360c0e9482d98d22ef50844547394b5cc4eb842d
MaribelC <Maribel_Calvario@yahoo.com>
2023-09-29 17:33:11 -0500

    corrected for loop

--- script.js
+++ script.js
@@ -5,7 +5,7 @@ function GetInfo(){
 
     fetch("https://api.openweathermap.org/data/2.5/forecast?q='+newName.value+'&appid=b63082b3af87e4200982c1544995286c")
 .then(response => response.json())
-.then(data =>){
+.then(data =>);{
     for(i=0;i<5;i++){
         document.getElementById("day" +(i+1)+"Min").innerHTML = "Min:" + Number(data.list[i].main.temp_min -288.53).toFixed(1)+"°";
     }
@@ -15,4 +15,4 @@ function GetInfo(){
     for(i=0;i<5;i++){
         document.getElementById("img" +(i+1).src) = "https://openweathermap.org/img/wn/" + data.list[i].weather[0].icon+".png";
     }
-}
+}}
